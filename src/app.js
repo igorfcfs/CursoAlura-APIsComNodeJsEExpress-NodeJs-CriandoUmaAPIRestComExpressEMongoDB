09a33cf56a30e70dc19b16d70f97282d811d8018
igorfcfs <igorfcfs@gmail.com>
2024-07-23 15:46:44 -0300

    Persistindo dados

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 import express from "express";
+import connect from "./config/dbConnect.js"
+import livro from "./models/Livros.js";
+
+const conexao = await connect();
+
+conexao.on("erro", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso!");
+});
 
 const app = express();
 app.use(express.json()); //middleware
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-];
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id === Number(id));
-}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
